61b6f89ceb7cf3fe072481a03f52dfb79cc30d72
Three Aims <james@3aims.com>
2014-07-23 19:00:08 +0100

    Remove bad redirect URLs

--- app.py
+++ app.py
@@ -147,11 +147,9 @@ def client():
         client_id=gen_salt(40),
         client_secret=gen_salt(50),
         _redirect_uris=' '.join([
-            'http://localhost:8000/authorized',
             'http://127.0.0.1:8000/authorized',
-            'http://127.0.1:8000/authorized',
-            'http://127.1:8000/authorized',
-            ]),
+            'http://localhost:8000/authorized',
+        ]),
         _default_scopes='email',
         user_id=user.id,
     )
